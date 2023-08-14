63dc2f602d16f336dd9559de207d11818a881c00
sravanthichampatruni <54381892+sravanthichampatruni@users.noreply.github.com>
2023-08-14 16:27:55 -0500

    Update Reverse.java

--- src/Reverse.java
+++ src/Reverse.java
@@ -3,9 +3,11 @@ public class Reverse {
         String s = "omsai ram";
         String rev = "";
         int len = s.length();
+         System.out.println("loop starts");
         for (int i = len-1;len>0; len--){
             rev = rev+ s.charAt(i);
         }
         System.out.println(rev);
+         System.out.println(rev);
     }
 }
