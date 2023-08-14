e419c240843a18e19b83c150e6d517f526b7c9ad
sravanthichampatruni <ch.sravanthi28@gmail.com>
2023-08-14 15:44:14 -0500

    first comment intelij

---
+++ src/reverse.java
@@ -0,0 +1,12 @@
+public class reverse {
+    public static void main(String [] args){
+        String s = "omsai ram";
+        String rev = "";
+        int len = s.length();
+        for (int i = len-1;len>0; len--){
+            rev = rev+ s.charAt(i);
+
+        }
+        System.out.println(rev);
+    }
+}
